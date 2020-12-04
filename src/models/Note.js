0f217326c9beb9a04f978159900787af613fa82f
moisesjusto <71886026+moisesjusto@users.noreply.github.com>
2020-12-04 15:14:27 -0400

    Update Note.js

--- src/models/Note.js
+++ src/models/Note.js
@@ -4,10 +4,10 @@ const noteSchema = new Schema(
     {
         id: { type: String},
         name: { type: String},
-        imgs: { type: String, required: true},
-        precio: { type: String, required: true},
-        categoria: { type: String, required: true},
-        descripsion: { type: String, required: true},
+        imgs: { type: String},
+        precio: { type: String},
+        categoria: { type: String},
+        descripsion: { type: String},
         
     }, {
         timestamps: true
